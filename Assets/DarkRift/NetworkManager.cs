860236edc317b2893dc4de778887aa532fa67f7b
Andrew Erskine <erskine94@gmail.com>
2021-01-10 14:52:23 -0800

    Message received, connect, disconnect

--- Assets/DarkRift/NetworkManager.cs
+++ Assets/DarkRift/NetworkManager.cs
@@ -29,14 +29,46 @@ public class NetworkManager : MonoBehaviour
     }
 
     void MessageReceived(object sender, MessageReceivedEventArgs e) {
-
+        using (Message message = e.GetMessage() as Message) {
+            if (message.Tag == Tags.PlayerConnectTag) {
+                PlayerConnect(sender, e);
+            } else if (message.Tag == Tags.PlayerDisconnectTag) {
+                PlayerDisconnect(sender, e);
+            }
+        }
     }
 
     void PlayerConnect(object sender, MessageReceivedEventArgs e) {
+        using (Message message = e.GetMessage()) {
+            using (DarkRiftReader reader = message.GetReader()) {
+                // Read the message data
+                ushort ID = reader.ReadUInt16();
+                string playerName = reader.ReadString();
 
+                // Player / Network Player Spawn
+                GameObject obj;
+                if (ID == drClient.ID) {
+                    // If this ID corresponds to this client, spawn the controllable player prefab
+                    obj = Instantiate(localPlayerPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+                } else {
+                    // Else we spawn a network prefab, non-controllable
+                    obj = Instantiate(networkPlayerPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+                }
+
+                // Get network entity data of prefab and add to network players store
+                networkPlayers.Add(ID, obj.GetComponent<NetworkEntity>());
+
+                // Update player name
+                networkPlayers[ID].SetPlayerName(playerName);
+            }
+        }
     }
 
     void PlayerDisconnect(object sender, MessageReceivedEventArgs e) {
-        
+        using (Message message = e.GetMessage()) {
+            using (DarkRiftReader reader = message.GetReader()) {
+                networkPlayers.Remove(reader.ReadUInt16());
+            }
+        }
     }
 }
