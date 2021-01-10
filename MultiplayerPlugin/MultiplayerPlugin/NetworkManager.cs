0e0bff35a9d00f61d364205cf3e70a9281d020cf
Andrew Erskine <erskine94@gmail.com>
2021-01-10 10:53:44 -0800

    Player class and connection methods

--- MultiplayerPlugin/MultiplayerPlugin/NetworkManager.cs
+++ MultiplayerPlugin/MultiplayerPlugin/NetworkManager.cs
@@ -12,8 +12,20 @@ namespace MultiplayerPlugin
     {
         public override bool ThreadSafe => false;
         public override Version Version => new Version(1, 0, 0);
+        Dictionary<IClient, Player> players = new Dictionary<IClient, Player>();
 
         public NetworkManager(PluginLoadData pluginLoadData) : base(pluginLoadData)
+        {
+            ClientManager.ClientConnected += ClientConnected;
+            ClientManager.ClientDisconnected += ClientDisconnected;
+        }
+
+        void ClientConnected(object sender, ClientConnectedEventArgs e)
+        {
+
+        }
+
+        void ClientDisconnected(object sender, ClientDisconnectedEventArgs e)
         {
 
         }
---
+++ MultiplayerPlugin/MultiplayerPlugin/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplayerPlugin
+{
+    class Player
+    {
+        public ushort ID { get; set; }
+        public string playerName { get; set; }
+
+        public Player(ushort _ID, string _playerName)
+        {
+            ID = _ID;
+            playerName = _playerName;
+        }
+    }
+}
