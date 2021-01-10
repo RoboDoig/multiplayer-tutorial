f3391a9e85dc4c4bfddfd389dc3a08e8b88e96f7
Andrew Erskine <erskine94@gmail.com>
2021-01-10 10:33:49 -0800

    Plugin boilerplate

---
+++ MultiplayerPlugin/MultiplayerPlugin/NetworkManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DarkRift;
+using DarkRift.Server;
+
+namespace MultiplayerPlugin
+{
+    class NetworkManager : Plugin
+    {
+        public override bool ThreadSafe => false;
+        public override Version Version => new Version(1, 0, 0);
+
+        public NetworkManager(PluginLoadData pluginLoadData) : base(pluginLoadData)
+        {
+
+        }
+    }
+}
