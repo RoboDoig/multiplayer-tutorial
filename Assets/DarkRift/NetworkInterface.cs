98b0c12c0cb9b44c5a3c293168df284a5cfb4586
Andrew Erskine <erskine94@gmail.com>
2021-01-10 12:31:11 -0800

    Basic network interface

---
+++ Assets/DarkRift/NetworkInterface.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DarkRift;
+using DarkRift.Client;
+using DarkRift.Client.Unity;
+
+public class NetworkInterface : MonoBehaviour
+{
+    public static NetworkInterface singleton;
+    private UnityClient drClient;
+
+    void Awake() {
+        if (singleton != null) {
+            Destroy(gameObject);
+            return;
+        }
+
+        singleton = this;
+    }
+
+    void Start() {
+        drClient = GetComponent<UnityClient>();
+    }
+
+    // Connect with local test server //
+    public void StartLocalSession() {
+        // Connect to local network
+        drClient.ConnectInBackground(drClient.Host, drClient.Port, drClient.Port, true, delegate {OnLocalSessionCallback();} );
+
+        // Update UI
+        UIManager.singleton.SetInputInteractable(false);
+    }
+
+    public void OnLocalSessionCallback() {
+        if (drClient.ConnectionState == ConnectionState.Connected) {
+            // Set lobby controls to interactable
+            UIManager.singleton.SetLobbyInteractable(true);
+        } else {
+            // Else reset the input UI
+            UIManager.singleton.SetInputInteractable(true);
+        }
+    }
+}
